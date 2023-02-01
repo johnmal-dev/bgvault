13c65a972fca372a296694c3f8139cc8287c00c6
John Malapit <johncmalapit@gmail.com>
2023-01-31 23:15:50 -0500

    remove unused useEffect import

--- src/Components/GameForm.jsx
+++ src/Components/GameForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const GameForm = ({ getGames }) => {
   const [gameInput, setGameInput] = useState('');
